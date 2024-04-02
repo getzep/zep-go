5dfb7cb0c7977d013aef1782da388cd17672c96c
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-04-02 01:54:35 +0000

    Release v0.0.4

--- core/request_option.go
+++ core/request_option.go
@@ -50,7 +50,7 @@ func (r *RequestOptions) cloneHeader() http.Header {
 	headers := r.HTTPHeader.Clone()
 	headers.Set("X-Fern-Language", "Go")
 	headers.Set("X-Fern-SDK-Name", "github.com/getzep/zep-go")
-	headers.Set("X-Fern-SDK-Version", "v0.0.3")
+	headers.Set("X-Fern-SDK-Version", "v0.0.4")
 	return headers
 }
 
