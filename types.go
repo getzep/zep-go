1245354d128b0bb80e26c8b63c1e3d235a6f4ac2
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-10-15 23:27:59 +0000

    SDK regeneration

--- types.go
+++ types.go
@@ -657,15 +657,15 @@ func (e EpisodeType) Ptr() *EpisodeType {
 }
 
 type Fact struct {
-	CreatedAt      *string  `json:"created_at,omitempty" url:"created_at,omitempty"`
+	CreatedAt      string   `json:"created_at" url:"created_at"`
 	ExpiredAt      *string  `json:"expired_at,omitempty" url:"expired_at,omitempty"`
-	Fact           *string  `json:"fact,omitempty" url:"fact,omitempty"`
+	Fact           string   `json:"fact" url:"fact"`
 	InvalidAt      *string  `json:"invalid_at,omitempty" url:"invalid_at,omitempty"`
 	Name           *string  `json:"name,omitempty" url:"name,omitempty"`
 	Rating         *float64 `json:"rating,omitempty" url:"rating,omitempty"`
 	SourceNodeName *string  `json:"source_node_name,omitempty" url:"source_node_name,omitempty"`
 	TargetNodeName *string  `json:"target_node_name,omitempty" url:"target_node_name,omitempty"`
-	UUID           *string  `json:"uuid,omitempty" url:"uuid,omitempty"`
+	UUID           string   `json:"uuid" url:"uuid"`
 	ValidAt        *string  `json:"valid_at,omitempty" url:"valid_at,omitempty"`
 
 	extraProperties map[string]interface{}
